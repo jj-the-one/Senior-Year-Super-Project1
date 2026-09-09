d40da5a5ff227325e76a89c389f6113a43135285
Jung-Jae Yoon <jjaeyoon008@gmail.com>
2026-09-09 10:01:27 -0700

    fs;.djf;sdkfj

--- A.I Spotting Game/Assets/Scripts/Scene Controller.cs
+++ A.I Spotting Game/Assets/Scripts/Scene Controller.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
@@ -14,5 +15,10 @@ public class SceneController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void StartGame()
+    {
+        SceneManager.LoadScene("Beginner Stage");
     }
 }
