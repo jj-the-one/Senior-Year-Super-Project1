d55f61cfa03e8ffdd5d4e773e147a26c967027f8
Jung-Jae Yoon <jjaeyoon008@gmail.com>
2026-09-16 10:18:28 -0700

    Jung-Jae Yoon 10:17 9/16/2026
    Added some pseudo code to Grading Display script. Not finished yet.

--- A.I Spotting Game/Assets/Scripts/Grading Display.cs
+++ A.I Spotting Game/Assets/Scripts/Grading Display.cs
@@ -4,10 +4,53 @@ using UnityEngine;
 
 public class GradingDisplay : MonoBehaviour
 {
+    //take in score from the player object/prefab + Player Inventory script.
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+        if(score <= 3){
+            Display: Your Score: (Score Variable)
+            Display: Grade: G
+            Display: Explanation: Getting a grade of G indicates a extremely high need of improvement as your ability to distinguish A.I and real imagery with a reference is lacking. 
+        }
+        else if(score <= 5){
+            Display: your score: (Score Variable)
+            Display: Grade: F
+            Display: Explanation: Getting a grade of F indicates a very high need of improvement as your ability to distinguish A.I and real imagery with a reference is lacking. 
+        }
+        else if(score <= 7){
+            Display: your score: (Score Variable)
+            Display: Grade: E
+            Display: Explanation: Getting a grade of E indicates a high need of improvement as your ability to distinguish A.I and real imagery with a reference is lacking.
+        }
+        else if(score <= 10){
+            Display: your score: (Score Variable)
+            Display: Grade: D
+            Display: Explanation: Getting a grade of D indicates room for improvement. From the previous level, you have demonstrated clear ability to  
+        }
+        else if(score <= 12){
+            Display: your score: (Score Variable)
+            Display: Grade: C
+        }
+        else if(score <= 14){
+            Display: your score: (Score Variable)
+            Display: Grade: B
+        }
+        else if(score <= 17){
+            Display: your score: (Score Variable)
+            Display: Grade: A
+        }
+        else if(score <= 19){
+            Display: your score: (Score Variable)
+            Display: Grade: S
+        }
+        else{
+            Display: your score: (Score Variable)
+            Display: Grade: Z
+        }
+         */
     }
 
     // Update is called once per frame
