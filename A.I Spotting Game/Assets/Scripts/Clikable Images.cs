fa835c6d194e19586b20c9d5d80d6659dbb8dcbb
Tvistar <danyashashkov08@gmail.com>
2026-09-18 10:35:32 -0700

    Danil Shashkov 09/18/26 Pseudo code for beginner
    Made a psedo code in java and prompted it in chatgpt. Still needs some adjustments. ArrayList and the currentimage.

--- A.I Spotting Game/Assets/Scripts/Clikable Images.cs
+++ A.I Spotting Game/Assets/Scripts/Clikable Images.cs
@@ -13,6 +13,32 @@ public class ClikableImages : MonoBehaviour, IPointerClickHandler
 /*
 if the image was clicked then it has to initiate sequence of actoons:
     -Random change of text
-    -Showing whic answer was right based on the task for the round
-    -
+    -Showing which answer was right based on the task for the round
+    -lookin for images in the arraylist of real and ai images.
+===================================================================
+
+    The code:
+    private PlayerInventory inventory;
+    GameObject player = GameObject.FindGameObjectWithTag("Player");
+    PlayerInventory inventory = player.GetComponent<PlayerInventory>();
+
+    bool rightAnswer = false;
+
+    for (int i = 0; i < beginnerimage.Count; i++)
+    {
+        if (beginnerimage[i] == currentImage)
+        {
+            rightAnswer = true;
+            break;
+        }
+    }
+
+    if (rightAnswer)
+    {
+        inventory.AddScore(1);
+    }
+    else
+    {
+        inventory.TakeDamage(1);
+    }
 */
