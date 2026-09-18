173efa26887419a35a4497c8595f575da993d80d
Jung-Jae Yoon <jjaeyoon008@gmail.com>
2026-09-18 10:16:48 -0700

    Jung-Jae Yoon 9/18/2026 10:16
    Added scripts

---
+++ A.I Spotting Game/Assets/Scripts/Player Display.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class PlayerDisplay : MonoBehaviour
+{
+    private PlayerInventory inventory;
+
+    public TMP_Text healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
