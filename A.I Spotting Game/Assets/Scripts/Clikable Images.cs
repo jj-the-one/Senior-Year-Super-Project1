ce77b0fe6e3c16318341bbaadbb61ae76a903845
Tvistar <danyashashkov08@gmail.com>
2026-09-23 10:33:56 -0700

    Clickabel images Script Beginner Level 9/23
    Images are spawning now and rescaling still doesn't quiet work.

--- A.I Spotting Game/Assets/Scripts/Clikable Images.cs
+++ A.I Spotting Game/Assets/Scripts/Clikable Images.cs
@@ -62,5 +62,26 @@ public class ClikableImages : MonoBehaviour
             leftImage.sprite = aiImage;
             rightImage.sprite = realImage;
         }
+        FitImageToScreen(leftImage);
+        FitImageToScreen(rightImage);
+    }
+    void FitImageToScreen(Image image)
+    {
+    image.SetNativeSize();
+
+    RectTransform rect = image.GetComponent<RectTransform>();
+
+    float screenWidth = Screen.width;
+    float screenHeight = Screen.height;
+
+    float widthScale = screenWidth / rect.rect.width;
+    float heightScale = screenHeight / rect.rect.height;
+
+    float scale = Mathf.Min(widthScale, heightScale);
+
+    // Don't enlarge images that are already smaller than the screen
+    scale = Mathf.Min(scale, 1f);
+
+    rect.localScale = new Vector3(scale, scale, 1f);
     }
 }
